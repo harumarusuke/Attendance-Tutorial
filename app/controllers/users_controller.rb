6be43c807da76971bfe7467085940205a27abca2
harumaru <harumarusuke@gmail.com>
2020-04-26 07:35:13 +0000

    8.3終了

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,9 @@
 class UsersController < ApplicationController
+  
+  before_action :set_user,only: [:show,:edit,:update]
+  before_action :logged_in_user,only:[:show,:edit,:update]
+  before_action :correct_user,only:[:edit,:update]
+  
   def new
     @user=User.new
   end
@@ -33,7 +38,25 @@ class UsersController < ApplicationController
   end
   
   private
+  
     def user_params
       params.require(:user).permit(:name,:email,:password,:password_confirmation)
     end
+    
+  def set_user
+    @user=User.find(params[:id])
+  end
+    
+  def logged_in_user
+    unless logged_in?
+      store_location
+      flash[:danger] = "ログインしてください。"
+      redirect_to login_url
+    end
+  end
+  
+  def correct_user
+    redirect_to(root_url) unless current_user?(@user)
+  end
+    
 end
