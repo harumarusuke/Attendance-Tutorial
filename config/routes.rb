6680070f42cf942d24420fedace1d2d02953a3cd
harumaru <harumarusuke@gmail.com>
2020-04-26 01:12:27 +0000

    ログインページ

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
 
+  get 'sessions/new'
+
   root 'static_pages#top'
   get '/signup',to: 'users#new'
+  
+  get '/login',to: 'sessions#new'
+  post '/login',to: 'sessions#create'
+  delete '/login',to: 'sessions#destroy'
+  
+  
+  
   resources :users
 end
